ccc5188e975c1d39f1806c1acdb55c6daddd44ba
Jose Cruzado <josecruzado2103@gmail.com>
2026-05-05 23:32:46 -0500

    feat: deterministic cudnn

--- espnet2/torch_utils/set_all_random_seed.py
+++ espnet2/torch_utils/set_all_random_seed.py
@@ -8,3 +8,5 @@ def set_all_random_seed(seed: int):
     random.seed(seed)
     np.random.seed(seed)
     torch.random.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
