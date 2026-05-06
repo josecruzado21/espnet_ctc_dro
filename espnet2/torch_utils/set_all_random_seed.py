527e7a0cce8f7cbd23801e90aa9ba566c663fdeb
Jose Cruzado <josecruzado2103@gmail.com>
2026-05-06 12:47:21 -0500

    feat: deterministic

--- espnet2/torch_utils/set_all_random_seed.py
+++ espnet2/torch_utils/set_all_random_seed.py
@@ -1,5 +1,6 @@
 import os
 import random
+import warnings
 
 import numpy as np
 import torch
@@ -10,6 +11,7 @@ def set_all_random_seed(seed: int):
     np.random.seed(seed)
     torch.random.manual_seed(seed)
     os.environ.setdefault("CUBLAS_WORKSPACE_CONFIG", ":4096:8")
-    torch.use_deterministic_algorithms(True)
+    torch.use_deterministic_algorithms(True, warn_only=True)
+    warnings.filterwarnings("ignore", message=".*does not have a deterministic implementation.*")
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
